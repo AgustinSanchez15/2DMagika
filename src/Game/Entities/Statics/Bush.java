e24e8b99b9e356348b9a4afc6b87ec48f5809213
Jose L Vera <Jose L Vera@LAPTOP-K64896NQ.lan>
2018-05-02 23:18:40 -0400

    still fixing bush

--- src/Game/Entities/Statics/Bush.java
+++ src/Game/Entities/Statics/Bush.java
@@ -1,25 +1,25 @@
-package Game.Entities.Statics;
-
-import java.awt.Graphics;
-
-import Game.Items.Item;
-import Main.Handler;
-import Resources.Images;
-
-public class Bush extends Tree{
-
-	public Bush(Handler handler, float x, float y) {
-		super(handler, x, y);
-		health = 8;
-	}
-	@Override
-    public void render(Graphics g) {
-        renderLife(g);
-        g.drawImage(Images.blocks[13],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
-
-    }
-	@Override
-    public void die() {
-        handler.getWorld().getItemManager().addItem(Item.stickItem.createNew((int)x + bounds.x,(int)y + bounds.y,1));
-    }
-}
+//package Game.Entities.Statics;
+//
+//import java.awt.Graphics;
+//
+//import Game.Items.Item;
+//import Main.Handler;
+//import Resources.Images;
+//
+//public class Bush extends Tree{
+//
+//	public Bush(Handler handler, float x, float y) {
+//		super(handler, x, y);
+//		health = 8;
+//	}
+//	@Override
+//    public void render(Graphics g) {
+//        renderLife(g);
+//        g.drawImage(Images.blocks[15],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+//
+//    }
+//	@Override
+//    public void die() {
+//        handler.getWorld().getItemManager().addItem(Item.stickItem.createNew((int)x + bounds.x,(int)y + bounds.y,1));
+//    }
+//}
