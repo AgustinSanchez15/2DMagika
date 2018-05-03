b49f60045e743236a18e1ab32ad50f27083f5183
Jose L Vera <Jose L Vera@LAPTOP-K64896NQ.lan>
2018-05-03 00:07:07 -0400

    implemented bush

--- src/Game/Entities/Statics/Bush.java
+++ src/Game/Entities/Statics/Bush.java
@@ -1,25 +1,106 @@
-//package Game.Entities.Statics;
-//
-//import java.awt.Graphics;
-//
-//import Game.Items.Item;
-//import Main.Handler;
-//import Resources.Images;
-//
-//public class Bush extends Tree{
-//
-//	public Bush(Handler handler, float x, float y) {
-//		super(handler, x, y);
-//		health = 8;
-//	}
-//	@Override
-//    public void render(Graphics g) {
-//        renderLife(g);
-//        g.drawImage(Images.blocks[15],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
-//
-//    }
-//	@Override
-//    public void die() {
-//        handler.getWorld().getItemManager().addItem(Item.stickItem.createNew((int)x + bounds.x,(int)y + bounds.y,1));
-//    }
-//}
+package Game.Entities.Statics;
+
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import Game.Items.Item;
+import Game.Tiles.Tile;
+import Main.Handler;
+import Resources.Images;
+
+public class Bush extends StaticEntity{
+	private File audioFile;
+    private AudioInputStream audioStream;
+    private AudioFormat format;
+    private DataLine.Info info;
+    private Clip audioClip;
+	public Bush(Handler handler, float x, float y) {
+		super(handler, x, y, Tile.TILEHEIGHT+20, Tile.TILEWIDTH+20);
+        bounds.x=20;
+        bounds.y=40;
+        bounds.width = 65;
+        bounds.height = 24;
+        health=8;
+
+        try {
+            audioFile = new File("res/music/Chopping.wav");
+            audioStream = AudioSystem.getAudioInputStream(audioFile);
+            format = audioStream.getFormat();
+            info = new DataLine.Info(Clip.class, format);
+            audioClip = (Clip) AudioSystem.getLine(info);
+            audioClip.open(audioStream);
+            audioClip.setMicrosecondPosition(2000);
+
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public void tick() {
+        if(isBeinghurt()){
+            audioClip.start();
+        }
+        if(!isBeinghurt() && !handler.getKeyManager().attbut){
+            audioClip.stop();
+        }
+        if(!isActive()){
+            audioClip.stop();
+
+        }
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        renderLife(g);
+        g.drawImage(Images.blocks[15],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+
+    }
+
+
+
+    @Override
+    public void die() {
+        handler.getWorld().getItemManager().addItem(Item.stickItem.createNew((int)x + bounds.x,(int)y + bounds.y,1));
+
+
+    }
+
+    public void renderLife(Graphics g) {
+        if (beinghurt && count <=8){
+            if(count == 8){
+                count = 0;
+                beinghurt=false;
+            }
+
+            g.drawImage(Images.numbers[getHealth()],(int)(x-handler.getGameCamera().getxOffset()+bounds.x),(int)(y-handler.getGameCamera().getyOffset()-getHeight()+(bounds.height+32)),42,42,null);
+            count++;
+
+        }
+    }
+
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
