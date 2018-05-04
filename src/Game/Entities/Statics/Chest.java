c33ca762adb1ee616c317a88c48e94a49587b86e
Jose L Vera <Jose L Vera@LAPTOP-K64896NQ.lan>
2018-05-04 13:02:43 -0400

    polished chest

--- src/Game/Entities/Statics/Chest.java
+++ src/Game/Entities/Statics/Chest.java
@@ -11,60 +11,65 @@ import Main.Handler;
 import Resources.Images;
 
 public class Chest extends StaticEntity{
-	
+
 	private Rectangle ir = new Rectangle();
-    public Boolean EP = false;
-    
+	public Boolean EP = false;
+	public Boolean isOpen = false;
+
 	public Chest(Handler handler, float x, float y) {
 		super(handler, x, y, Tile.TILEHEIGHT, Tile.TILEWIDTH);
 		health=10000000;
-        bounds.width = Tile.TILEWIDTH;
-        bounds.height = Tile.TILEHEIGHT;
-        ir.width = bounds.width;
-        ir.height = bounds.height;
-        int irx=(int)(bounds.x-handler.getGameCamera().getxOffset()+x);
-        int iry= (int)(bounds.y-handler.getGameCamera().getyOffset()+y+height/2);
-        ir.y=iry;
-        ir.x=irx;
+		bounds.width = Tile.TILEWIDTH;
+		bounds.height = Tile.TILEHEIGHT;
+		ir.width = bounds.width;
+		ir.height = bounds.height;
+		int irx=(int)(bounds.x-handler.getGameCamera().getxOffset()+x);
+		int iry= (int)(bounds.y-handler.getGameCamera().getyOffset()+y+height/2);
+		ir.y=iry;
+		ir.x=irx;
 	}
 
 	@Override
 	public void tick() {
-		
+
 		if(isBeinghurt()){
-            setHealth(10000000);
-        }
+			setHealth(10000000);
+		}
 
-        if(handler.getKeyManager().attbut){
-            EP=true;
+		if(handler.getKeyManager().attbut){
+			EP=true;
 
-        }else if(!handler.getKeyManager().attbut){
-            EP=false;
-        }
+		}else if(!handler.getKeyManager().attbut){
+			EP=false;
+		}
 
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Images.chest[0],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
-		
-        checkForPlayer(g, handler.getWorld().getEntityManager().getPlayer());
+		if(!isOpen) {
+			g.drawImage(Images.chest[0],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+		}else if(isOpen) {
+			g.drawImage(Images.chest[1],(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+		}
+		checkForPlayer(g, handler.getWorld().getEntityManager().getPlayer());
 	}
 	private void checkForPlayer(Graphics g, Player p) {
-        Rectangle pr = p.getCollisionBounds(0,0);
+		Rectangle pr = p.getCollisionBounds(0,0);
 
-        if(ir.contains(pr) && !EP){
-            g.drawImage(Images.E,(int) x+width,(int) y+10,32,32,null);
-        }else if(ir.contains(pr) && EP){
-            g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
-           
-        }
+		if(ir.contains(pr) && !EP){
+			g.drawImage(Images.E,(int) x+width,(int) y+10,32,32,null);
+		}else if(ir.contains(pr) && EP){
+			g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
+			isOpen = true;
 
-
-    }
+		}
+		if(ir.contains(pr) && !EP && isOpen) {
+			g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
+		}
+	}
 	@Override
 	public void die() {
-		
-		
+
 	}
 }
