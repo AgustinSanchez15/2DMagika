c00cda571d728fd76e9c3df4504abf529584c89b
Agustin Sanchez <agustin.sanchez@upr.edu>
2018-05-08 14:24:16 -0400

    added a sound effect when player loses

--- src/Game/GameStates/LoseState.java
+++ src/Game/GameStates/LoseState.java
@@ -2,55 +2,86 @@ package Game.GameStates;
 
 
 
-import Main.Game;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import Main.Handler;
 import Resources.Images;
 import UI.ClickListlener;
 import UI.UIImageButton;
 import UI.UIManager;
-import Main.Handler;
-
-import java.awt.*;
 
 /**
  * Created by Elemental on 12/10/2016.
  */
 public class LoseState extends State {
 
-    private UIManager uiManager;
+	private UIManager uiManager;
+	private File audioFile;
+	private AudioInputStream audioStream;
+	private AudioFormat format;
+	private DataLine.Info info;
+	private Clip audioClip;
 
-    public LoseState(Handler handler) {
-        super(handler);
-        uiManager = new UIManager(handler);
-        handler.getMouseManager().setUimanager(uiManager);
+	public LoseState(Handler handler) {
+		super(handler);
+		uiManager = new UIManager(handler);
+		handler.getMouseManager().setUimanager(uiManager);
 
 
-        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-64, handler.getHeight()/2+64, 128, 64, Images.tryAgainbtn, new ClickListlener() {
-            @Override
-            public void onClick() {
-                handler.getMouseManager().setUimanager(null);
-                State.setState(handler.getGame().menuState);
-            }
-        }));
-    }
+		uiManager.addObjects(new UIImageButton(handler.getWidth()/2-64, handler.getHeight()/2+64, 128, 64, Images.tryAgainbtn, new ClickListlener() {
+			@Override
+			public void onClick() {
+				handler.getMouseManager().setUimanager(null);
+				State.setState(handler.getGame().menuState);
+			}
+		}));
+		try {
+			audioFile = new File("res/music/game_over.wav");
+			audioStream = AudioSystem.getAudioInputStream(audioFile);
+			format = audioStream.getFormat();
+			info = new DataLine.Info(Clip.class, format);
+			audioClip = (Clip) AudioSystem.getLine(info);
+			audioClip.open(audioStream);
 
-    @Override
-    public void tick() {
-        handler.getMouseManager().setUimanager(uiManager);
-        uiManager.tick();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
 
-        // Temporarily just go directly to the GameState, skip the menu state!
-        //handler.getMouseManager().setUimanager(null);
-        //State.setState(handler.getGame().gameState);
-    }
+	@Override
+	public void tick() {
+		handler.getMouseManager().setUimanager(uiManager);
+		uiManager.tick();
+		audioClip.start();
 
-    @Override
-    public void render(Graphics g) {
-        g.setColor(Color.darkGray);
-        g.fillRect(0,0,handler.getWidth(),handler.getHeight());
-        g.drawImage(Images.loseScreen,0,0,800,600,null);
-        uiManager.Render(g);
+		// Temporarily just go directly to the GameState, skip the menu state!
+		//handler.getMouseManager().setUimanager(null);
+		//State.setState(handler.getGame().gameState);
+	}
 
-    }
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.darkGray);
+		g.fillRect(0,0,handler.getWidth(),handler.getHeight());
+		g.drawImage(Images.loseScreen,0,0,800,600,null);
+		uiManager.Render(g);
+
+	}
 
 
 }
